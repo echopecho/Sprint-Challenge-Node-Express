639373564209228c0186194e60c7bbdca156d6fd
echopecho <pbsmithtbn@gmail.com>
2019-03-09 12:06:17 -0600

    Add final Project CRUD methods
    I completed the put and delete requests for the project router. I also added in one that would return the actions associated with a specific project.

--- projects/project-router.js
+++ projects/project-router.js
@@ -23,14 +23,61 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.get('/:id/actions', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const actions = await Project.getProjectActions(id);
+    if(actions.length > 0) {
+      res.status(200).json(actions);
+    } else {
+      res.status(404).json({error: "There are no actions."})
+    }
+    
+  } catch (e) {
+    res.status(500).json({err: "Something went wrong with the server."})
+  }
+})
+
 router.post('/', async (req, res) => {
   const newProject = req.body;
 
   try {
     const project = await Project.insert(newProject);
-    res.status(200).json(project);
+    res.status(201).json(project);
   } catch (e) {
     res.status(500).json({err: "Something went wrong with the server."});
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const updatedProject = req.body;
+
+  try {
+    const project = await Project.update(id, updatedProject);
+    if(project) {
+      res.status(201).json(project);
+    } else {
+      res.status(400).json({error: "ID does not exist"})
+    }
+  } catch (e) {
+    res.status(500).json({err: "Something went wrong with the server."})
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const count = await Project.remove(id);
+    if(count === 1) {
+      res.status(201).json({message: "Project deleted."})
+    } else {
+      res.status(404).json({error: "ID does not exist"})
+    }
+  } catch (e) {
+    res.status(500).json({err: "Something went wrong with the server."})
   }
 })
 
