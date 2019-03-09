f3218793770405645b853e36c2a8228bfab1d05b
echopecho <pbsmithtbn@gmail.com>
2019-03-09 11:51:22 -0600

    Add post request to Project endpoint
    I added the router to allow new projects to be added. I also made it so you can retrieve projects based on id.

--- projects/project-router.js
+++ projects/project-router.js
@@ -10,6 +10,28 @@ router.get('/', async (req, res) => {
   } catch (e) {
     res.status(500).json({err: "Something went wrong with the server."})
   }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const project = await Project.get(id);
+    res.status(200).json(project);  
+  } catch (e) {
+    res.status(500).json({err: "Something went wrong with the server."})
+  }
+});
+
+router.post('/', async (req, res) => {
+  const newProject = req.body;
+
+  try {
+    const project = await Project.insert(newProject);
+    res.status(200).json(project);
+  } catch (e) {
+    res.status(500).json({err: "Something went wrong with the server."});
+  }
 })
 
 module.exports = router;
